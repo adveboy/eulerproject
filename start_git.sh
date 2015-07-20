ff2f6da8a10a82aee2cf4f4f19c61b536e363ebe
Sergey Kardash <skardash@gmail.com>
2015-07-20 07:51:16 +0300

    ololo

--- start_git.sh
+++
@@ -1,6 +0,0 @@
-echo "# eulerproject" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/adveboy/eulerproject.git
-git push -u origin master
